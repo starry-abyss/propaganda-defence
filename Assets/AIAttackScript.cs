d4f3273547b88b2454f4c42f7016a436e4d21382
scorched <scorched@bk.ru>
2015-04-18 12:17:13 +0300

    Initial commit. Done some pathfinding.

---
+++ Assets/AIAttackScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIAttackScript : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<NavMeshAgent>().SetDestination(target.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//GetComponent<NavMeshAgent>().velocity = ;
+	}
+}
